c1674ce29d75175becc06cd13d9f1f0f733fbfd3
Manuel Nascimento <nerdbroz2@gmail.com>
2021-07-13 02:59:14 +0000

    chore: finish user service

--- src/services/UserService.php
+++ src/services/UserService.php
@@ -2,6 +2,7 @@
 
 namespace services;
 
+use entities\User;
 use repositories\UserRepository;
 
 class UserService
@@ -18,14 +19,31 @@ class UserService
         try {
             $list = $this->userRepository->index();
             array_map(function ($e) {
+                new User($e['id'], $e['email'], $e['username'], $e['fullname'], $e['password']);
             }, $list);
+            return $list;
         } catch (\Exception $e) {
+            echo "Error";
         }
     }
-    public function createUser()
-    {
+    public function createUser(
+        String $username,
+        String $password,
+        String $fullname,
+        String $email
+    ) {
+        try {
+            $this->userRepository->store($username, $password, $fullname, $email);
+        } catch (\Exception $e) {
+            echo "Error";
+        }
     }
-    public function getUser()
+    public function logIn(String $email, String $password)
     {
+        try {
+            $fetch = $this->userRepository->auth($email, $password);
+            return new User($fetch['id'], $fetch['email'], $fetch['username'], $fetch['fullname'], $fetch['password']);
+        } catch (\Exception $e) {
+        }
     }
 }

983c112056ed2a6492c3ffbf742d288a543bc45b
Manuel Nascimento <nerdbroz2@gmail.com>
2021-07-13 02:46:46 +0000

    chore: class construct

--- src/entities/User.php
+++ src/entities/User.php
@@ -9,8 +9,18 @@ class User
     private String $password;
     private String $fullname;
     private String $username;
+
+    function __construct(Int $id, String $email, String $username, String $fullname, String $password)
+    {
+        $this->id = $id;
+        $this->email = $email;
+        $this->username = $username;
+        $this->fullname = $fullname;
+        $this->password = $password;
+    }
+
     public function hashPassword($encriptation)
     {
-        $this->password = password_hash($this->password,$encriptation);
+        $this->password = password_hash($this->password, $encriptation);
     }
 }

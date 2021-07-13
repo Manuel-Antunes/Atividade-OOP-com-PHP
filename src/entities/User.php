c143fbb269ad6cbd99669571247f76cbea8152c4
Manuel Nascimento <nerdbroz2@gmail.com>
2021-07-13 03:02:40 +0000

    chore: add getters and setters

--- src/entities/User.php
+++ src/entities/User.php
@@ -23,4 +23,100 @@ class User
     {
         $this->password = password_hash($this->password, $encriptation);
     }
+    
+    /**
+     * Get the value of fullname
+     *
+     * @return  String
+     */
+    public function getFullname()
+    {
+        return $this->fullname;
+    }
+
+    /**
+     * Set the value of fullname
+     *
+     * @param  String  $fullname
+     *
+     * @return  self
+     */
+    public function setFullname(String $fullname)
+    {
+        $this->fullname = $fullname;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of id
+     *
+     * @return  Int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @param  Int  $id
+     *
+     * @return  self
+     */
+    public function setId(Int $id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of username
+     *
+     * @return  String
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Set the value of username
+     *
+     * @param  String  $username
+     *
+     * @return  self
+     */
+    public function setUsername(String $username)
+    {
+        $this->username = $username;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     *
+     * @return  String
+     */
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @param  String  $email
+     *
+     * @return  self
+     */
+    public function setEmail(String $email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
 }

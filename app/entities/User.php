a347bc5d8033824e965a30c708d5456a6b849438
Manuel Nascimento <57446204+Manuel-Antunes@users.noreply.github.com>
2021-07-13 16:20:49 -0300

    fix: remove password field from user entity

--- app/entities/User.php
+++ app/entities/User.php
@@ -4,22 +4,15 @@ class User
 {
     private Int $id;
     private String $email;
-    private String $password;
     private String $fullname;
     private String $username;
 
-    function __construct(Int $id, String $email, String $username, String $fullname, String $password)
+    function __construct(Int $id, String $email, String $username, String $fullname)
     {
         $this->id = $id;
         $this->email = $email;
         $this->username = $username;
         $this->fullname = $fullname;
-        $this->password = $password;
-    }
-
-    public function hashPassword($encriptation)
-    {
-        $this->password = password_hash($this->password, $encriptation);
     }
 
     /**

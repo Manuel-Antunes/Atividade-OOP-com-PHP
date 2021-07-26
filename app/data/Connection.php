66124ab214399a6df0e60127db810bb79f0f9ae8
Manuel Nascimento <nerdbroz2@gmail.com>
2021-07-26 16:21:56 +0000

    chore: integrate gitpod

--- app/data/Connection.php
+++ app/data/Connection.php
@@ -4,8 +4,8 @@ class Connection
     public static function getConnection()
     {
         $database = "atividade";
-        $username = "root";
-        $senha = "";
-        return new PDO("mysql:host=localhost;dbname=$database", $username, $senha);
+        $username = "manuel";
+        $senha = "123";
+        return new PDO("mysql:host=127.0.0.1;dbname=$database", $username, $senha);
     }
 }
